1ae890578edde42254eb6a53b171423307224ee8
Petar <mbg012@gmail.com>
2014-10-28 00:56:57 +0200

    contact form

--- inc/pages/contact-us.php
+++ inc/pages/contact-us.php
@@ -1,3 +1,32 @@
 <div class="container">
+    <form action="" method="post" class="form-horizontal" id="contact-form">
+        <fieldset>
+            <legend>Contact us</legend>
 
+            <div class="form-group">
+                <label for="name" class="col-md-2 control-label">Your name</label>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" id="name" placeholder="Name" required>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email" class="col-md-2 control-label">Your email</label>
+                <div class="col-md-4">
+                    <input type="email" class="form-control" id="email" placeholder="Email" required>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="message" class="col-md-2 control-label">Message</label>
+                <div class="col-md-6">
+                    <textarea class="form-control" rows="8" required></textarea>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <input class="btn btn-primary pull-right" type="submit" value="Submit">
+            </div>
+        </fieldset>
+    </form>
 </div>
