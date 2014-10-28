dea08b7a4d66bc899198912f3517070ab30f1589
SStoyanov22 <SStoyanov22@gmail.comcdgit config --global user.email SStoyanov22@gmail.com>
2014-10-28 09:33:21 +0200

    changed footer and finalized staff page

--- inc/layout/footer.php
+++ inc/layout/footer.php
@@ -9,20 +9,22 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor excepturi ut?</p>
             </div>
             <div class="col-md-4">
-                <h4>Footer</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, quam.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor excepturi ut?</p>
+                <h4>Owners</h4>
+                <ul>
+                    <li><a href="https://www.facebook.com/SStoyanov22" class="owners">Stoyan Stoyanov</a></li>
+                    <li><a href="https://www.facebook.com/sdfdsf.rtgreds" class="owners">Petar Petrov</a></li>
+                    <li><a href="https://www.facebook.com/velizar.terziev?fref=ts" class="owners">Velizar Terziev</a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100000600516012" class="owners">Angel Zlatenov</a></li>
+                </ul> 
             </div>
             <div class="col-md-4">
-                <h4>Footer</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, quam.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolor excepturi ut?</p>
+                <h4>Validators</h4>
+                <a href="http://validator.w3.org"><img class="validator" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML " width="88" height="31" /></a> &nbsp;&nbsp;
+                <a href="http://css-validator.org"><img class="validator"  src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /></a>
             </div>
         </div>
         <hr/>
-        <p>&copy; Team French Wine | <a href="https://softuni.bg">SoftUni</a></p>
+        <p><a href="https://github.com/SoftUniTeamWork/WebFundamentalsTeamWork">&copy; Team French Wine</a> | <a href="https://softuni.bg">SoftUni</a></p>
     </div>
 </footer>
 
--- inc/pages/staff.php
+++ inc/pages/staff.php
@@ -3,7 +3,7 @@
         <div class="col-md-4"><a href="images/staff/nakov1.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/nakov1.jpg" alt=""/></a></div>
         <div class="col-md-4">
             <h1>Dj Nako C#</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>           
+            <p>Svetlin Nakov a.k.a. Dj Nako C# has been named number one in the prestigious DJ MAG Top 100 poll four times in a row, as the first DJ in the history of the DJ Magazine and an indication of his prolific output over the more than 14 years he’s been dj’ing and producing. Having won the prestigious prize "John Atanasov" for his achievements in djing is just one of his many achievements, but the driving force behind his success is an overriding passion for dance music.</p>           
         </div>
         <div class="col-md-4"><a href="images/staff/nakov2.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/nakov2.jpg" alt=""/></a></div>
     </div>
@@ -18,7 +18,7 @@
         <div class="col-md-4"><a href="images/staff/Alex.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/Alex.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Hostess/Waitress</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Alexandra Alexandrova a.k.a. Alex The Punisher is the one that welcomes our guests at the entrance. When the club is full she helps serving drinks to our guests. She is known for her cruel methods when she gets in tough situations. Nobody knows anything about her past so better don't ask because you might get into trouble.</hp>
         </div>
     </div>
 
@@ -26,7 +26,7 @@
         <div class="col-md-4"><a href="images/staff/waitresses.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/waitresses.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Waitresses</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Here are our bambinas, chequitas, or in pure english- waitresses: Petya and Alex. They are our main attraction in the club. Most of the guest come here for the only reason to take a picture with them.</p>
         </div>
     </div>
 
@@ -34,7 +34,7 @@
         <div class="col-md-4"><a href="images/staff/kristianBarman.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/kristianBarman.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Barman</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Here is one of our barmaids - Kristian. He is known for his juggling skills. If you are bored you can go to the bar and have a talk with him he is really friendly guy.</p>
         </div>
     </div>
 
@@ -42,7 +42,7 @@
         <div class="col-md-4"><a href="images/staff/georgiBarman.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/georgiBarman.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Barman</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>This is our second barmaid - Georgi. He is a real gentlemen with the ladies and some of them come here just to have the opportunity to talk to him.</p>
         </div>
     </div>
 
@@ -50,21 +50,21 @@
         <div class="col-md-4"><a href="images/staff/tenchev.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/tenchev.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Chief of Security</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Mr.Tenchev is an ex gang member joined our crew as chief of security. Known to be a testosterone miracle. He hasn't competed in any competition so we can say how strong he is but let's say we've seen him take down walls and turn around cars.</p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4"><a href="images/staff/guard1.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/guard1.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Security</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Vladi G is an ex rapstar whose carrier went down after he got divorsed with his ex Nicki Minaj. After the divorse he started taking steroids and started working with us.</p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4"><a href="images/staff/guard2.jpg" data-lightbox="staff-lightbox"><img class="img-responsive" src="images/staff/guard2.jpg" alt=""/></a></div>
         <div class="col-md-8">
             <h1>Security</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio error explicabo ipsam reiciendis rem vero. At consequatur ducimus eaque harum in possimus. Debitis facere ipsam necessitatibus praesentium quibusdam, quidem. Similique!</p>
+            <p>Vladislav is an ex russian mob. He was in one of the most rotting prisons for 10 years where he was training every single day and became what he is.</p>
         </div>
     </div>
 </div>
