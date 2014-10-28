6841d80e0f23a7786c9c8ee1dd969a02f894ab45
SStoyanov22 <SStoyanov22@gmail.comcdgit config --global user.email SStoyanov22@gmail.com>
2014-10-28 15:01:08 +0200

    Final version

--- inc/pages/events.php
+++ inc/pages/events.php
@@ -1,5 +1,34 @@
 <div class="container">
-    <div class="jumbotron">
-        <h1>Events</h1>
-    </div>
+    <div id="home-slider" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#home-slider" data-slide-to="0" class="active"></li>
+            <li data-target="#home-slider" data-slide-to="1"></li>
+            <li data-target="#home-slider" data-slide-to="2"></li>
+            <li data-target="#home-slider" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="images/events/BlackStaffParty.jpg" alt="slide">
+                
+            </div>
+            <div class="item">
+                <img src="images/events/missShare.jpg" alt="slide">
+                
+            </div>
+            <div class="item">
+                <img src="images/events/halloween.jpg" alt="slide">
+               	
+            </div>
+
+            <!-- Controls -->
+	        <a class="left carousel-control" href="#home-slider" role="button" data-slide="prev">
+	            <span class="glyphicon glyphicon-chevron-left"></span>
+	        </a>
+	        <a class="right carousel-control" href="#home-slider" role="button" data-slide="next">
+	            <span class="glyphicon glyphicon-chevron-right"></span>
+	        </a>
+        </div>
 </div>
